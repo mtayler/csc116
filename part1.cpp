a66e95f149eec702a9c34ae9520bf38fecae8780
Tayler Mulligan <mulligantayler@gmail.com>
2015-10-25 18:56:27 -0700

    Vector tests pass

--- part1.cpp
+++ part1.cpp
@@ -2,33 +2,81 @@
 
 int vector_max (vector<int> &theList)
 {
-	return -1;
+	int max = INT_MIN;
+	for (unsigned int i=0; i < theList.size(); i++) {
+		if (theList[i] > max) {
+			max = theList[i];
+		}
+	}
+	return max;
 }
 
 int vector_min (vector<int> &theList)
 {
-	return -1;
+	int min = INT_MAX;
+	for (unsigned int i=0; i < theList.size(); i++) {
+		if (theList[i] < min) {
+			min = theList[i];
+		}
+	}
+	return min;
 }
 
 int vector_count_values (vector<int> &theList, int value)
 {
-	return -1;
+	unsigned int count = 0;
+
+	for (unsigned int i=0; i < theList.size(); i++) {
+		if (theList[i] == value) {
+			count++;
+		}
+	}
+	return count;
 }
 
 
 int list_max (list<int> &theList)
 {
-	return -1;
+	int max = INT_MIN;
+
+	list<int>::iterator i = theList.begin();
+
+	while (i != theList.end()) {
+		if (*i > max) {
+			max = *i;
+		}
+	}
+
+	return max;
 }
 
 int list_min (list<int> &theList)
 {
-	return -1;
+	int min = INT_MAX;
+
+	list<int>::iterator i = theList.begin();
+
+	while (i != theList.end()) {
+		if (*i < min) {
+			min = *i;
+		}
+	}
+
+	return min;
 }
 
 int list_count_values (list<int> &theList, int value)
 {
-	return -1;
+	unsigned int count = 0;
+
+	list<int>::iterator i = theList.begin();
+
+	while (i != theList.end()) {
+		if (*i == value) {
+			count++;
+		}
+	}
+	return count;
 }
 
 
